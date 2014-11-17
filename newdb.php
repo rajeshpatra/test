8c5ef1d7221070660401a00109cff0bc23334006
Rajesh Patra <p7.rajesh@gmail.com>
2014-11-17 22:20:28 +0530

    fixed selecting database

--- newdb.php
+++ newdb.php
@@ -1,11 +1,12 @@
 <?php
 	require "connection.php";
-	$query = "CREATE DATABASE BIODATA";
-	$biodata = mysql_query($db, $connect);
-	if(!$biodata) {
-		die('Could not create databse: ' . mysql_error());
-	}
-	echo "Database successfully created.";
+	// $query = "CREATE DATABASE BIODATA";
+	// $biodata = mysql_query($query, $connect);
+	// if(!$biodata) {
+	// 	die('Could not create databse: ' . mysql_error());
+	// }
+	// echo "Database successfully created.";
+
 	$query1 = mysql_select_db('BIODATA');
 	if (!$query1) {
 		die('Could not select database: ' . mysql_error());
